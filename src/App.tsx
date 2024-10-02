965b5b7191a78029f247860a5d746b85453bbb63
ylyas <62172641+ylyas2004@users.noreply.github.com>
2024-10-02 23:04:21 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './tdc.css';
 import Navbar from './views/Navbar';
-import Home from './views/Home';
 import Portfolio from './views/Portfolio';
 import About from './views/About';
 import Footer from './views/Footer';
